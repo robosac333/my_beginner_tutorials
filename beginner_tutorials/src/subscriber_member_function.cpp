201be108c365d08611d14797749be2699c4427f9
robosac333 <sjd3333@umd.edu>
2024-11-06 22:22:35 -0500

    updated the code with appropriate doxygen comments

--- beginner_tutorials/src/subscriber_member_function.cpp
+++ beginner_tutorials/src/subscriber_member_function.cpp
@@ -1,16 +1,17 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * @file MinimalSubscriber.cpp
+ * @brief Implementation of the MinimalSubscriber class for a ROS2 node that subscribes to a topic.
+ * @version 0.1
+ * @date 2024-11-06
+ *
+ * @author
+ * Sachin Jadhav (sjd3333@umd.edu)
+ *
+ * @copyright
+ * Copyright (c) 2024 Sachin Jadhav
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
 
 #include <functional>
 #include <memory>
@@ -20,20 +21,51 @@
 
 using std::placeholders::_1;
 
+/**
+ * @class MinimalSubscriber
+ * @brief A simple subscriber node in ROS2 that listens to a String message topic.
+ *
+ * This class creates a ROS2 node that subscribes to a topic named "topic" and logs the received messages.
+ */
 class MinimalSubscriber : public rclcpp::Node {
  public:
+  /**
+   * @brief Construct a new Minimal Subscriber node.
+   *
+   * The constructor initializes the node and sets up a subscription to the "topic" topic.
+   */
   MinimalSubscriber() : Node("minimal_subscriber") {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
         "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
  private:
+  /**
+   * @brief Callback function to process received messages.
+   *
+   * This function is triggered each time a new message is received on the subscribed topic.
+   * It logs the content of the message.
+   *
+   * @param msg The message received from the topic, of type std_msgs::msg::String.
+   */
   void topic_callback(const std_msgs::msg::String& msg) const {
     RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
   }
+
+  /// Subscription object for receiving messages from the topic.
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
+/**
+ * @brief Main function for the MinimalSubscriber node.
+ *
+ * Initializes the ROS2 system, creates the MinimalSubscriber node, and starts
+ * spinning to process callbacks.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return int Exit status of the program.
+ */
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
